59f71b89d8019271eb711f6a3c3120d6dbd78195
Kyle Baran <kbaran@bitscoop.com>
2018-06-04 14:14:23 -0700

    fixup

--- components/search.vue
+++ components/search.vue
@@ -539,7 +539,7 @@
         this.closeFilterEditor();
         this.$store.state.facetSelectOpen = false;
 
-        if (this.$store.state.searching === true) {
+        if (this.$store.state.searching === true || this.$store.state.searchEnded === true) {
           return;
         }
 
@@ -670,9 +670,13 @@
         }
 
         this.$store.state.offset += this.$store.state.pageSize;
-        this.$store.state.searchEnded = this.$store.state.objects.events.length < this.$store.state.pageSize;
+        this.$store.state.searchEnded = this.$store.state.objects[facet].length < this.$store.state.pageSize;
         this.$store.state.searching = false;
         this.$store.state.spinner = false;
+
+        console.log(this.$store.state.objects[facet].length);
+        console.log(this.$store.state.pageSize);
+        console.log(this.$store.state.searchEnded);
       },
 
       compactOverflowFilters: function() {
