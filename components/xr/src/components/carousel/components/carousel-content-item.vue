02f4ac4e711a1fdb6e9c2facbf5be9624a32d623
mohrtw <mohrtw@gmail.com>
2018-06-07 15:09:06 -0700

    began improving xr content

--- components/xr/src/components/carousel/components/carousel-content-item.vue
+++ components/xr/src/components/carousel/components/carousel-content-item.vue
@@ -1,86 +1,94 @@
 <template>
-    <!-- <a-entity class="carousel-item carousel-content-item">
-        <a-entity scale="2 2 1"
-                  :text="this.textString(obj.title)"
-                  position="0 -.2 0"/>
-        <a-entity scale="2 2 1"
-                  :text="this.textString('type: ' + obj.type)"
-                  position="0 -.4 0"/>
-        <a-entity scale="2 2 1"
-                  :text="this.textString('url: ' + obj.url)"
-                   position="0 -.6 0"/>
-    </a-entity> -->
-    <a-entity class="carousel-item carousel-content-item" v-bind:id="obj.id">
+    <a-entity class="carousel-item carousel-content-item" v-bind:id="content.id">
 		<a-entity class="header">
 			<a-entity class="type">
-				<i v-bind:class="getContentTypeIcon(obj.type)"></i>
-                <a-entity
+				<!-- <i v-bind:class="getContentTypeIcon(content.type)"></i> -->
+                <!-- <a-entity
                     geometry="primitive: plane; width: 3; height: 2"
                     :material="this.imageMaterial"
                     rotation="0 0 0"
                     position="0 1 0">
-                </a-entity>
+                </a-entity> -->
                 <a-entity scale="2 2 1"
-                  :text="this.textString(obj.type)"
-                   position="0 -.6 0"/>
+                  :text="this.textString(content.type)"
+                   position="0 -.2 0"/>
 			</a-entity>
 
-			<div class="provider">
-				<i v-bind:class="getProviderIcon(connection.provider)"></i> {{ connection.name | truncate(30) }}
-			</div>
+			<a-entity v-if="connection.name !== undefined" class="provider">
+				<!-- <i v-bind:class="getProviderIcon(connection.provider)"></i> {{ connection.name | truncate(30) }} -->
+				<a-entity scale="2 2 1"
+                  :text="this.textString(connection.name)"
+                   position="0 -.4 0"/>
+			</a-entity>
 
-			<aside class="action-bar" v-on:click="openActionModal(content, 'content')">
-				<span>Tag</span><i class="fa fa-hashtag"></i>
-				<span>Share</span><i class="fa fa-share"></i>
-			</aside>
 		</a-entity>
 
-		<div class="content-embed" data-type="content" v-bind:data-id="content.id">
-      <audio v-if="isAudio(content)"controls v-bind:style="{ width: getWidth, height: getHeight }"><source v-bind:src="content.embed_content" v-bind:type="getAudioType(content.embed_format)"></audio>
-      <img v-if="isImage(content)" v-bind:src="content.embed_content" v-bind:alt="content.title"/>
-      <video v-if="isVideo(content)" v-bind:width="getWidth" v-bind:height="getHeight" controls><source v-bind:src="content.embed_content" v-bind:type="getVideoType(content.embed_format)"></video>
-      <iframe v-if="isEmail(content)" frameBorder="0" v-bind:width="getWidth()" v-bind:height="getHeight()" v-on:load="renderEmailIframe(content)" v-bind:name="content.id"></iframe>
-      <div v-if="isIframe(content)"><span v-html="content.embed_content"></span></div>
-    </div>
+		<!-- audio/image/video/email/iframe/-->
+		<!--  <a-entity class="content-embed" data-type="content" v-bind:data-id="content.id">
+		 	<audio v-if="isAudio(content)"controls v-bind:style="{ width: getWidth, height: getHeight }"><source v-bind:src="content.embed_content" v-bind:type="getAudioType(content.embed_format)"></audio>
+		 	<img v-if="isImage(content)" v-bind:src="content.embed_content" v-bind:alt="content.title"/>
+		 	<video v-if="isVideo(content)" v-bind:width="getWidth" v-bind:height="getHeight" controls><source v-bind:src="content.embed_content" v-bind:type="getVideoType(content.embed_format)"></video>
+		 	<iframe v-if="isEmail(content)" frameBorder="0" v-bind:width="getWidth()" v-bind:height="getHeight()" v-on:load="renderEmailIframe(content)" v-bind:name="content.id"></iframe>
+		 	<div v-if="isIframe(content)"><span v-html="content.embed_content"></span></div>
+		 </a-entity> -->
 
-		<div v-if="content.embed_thumbnail && !isImage(content) && !isVideo(content) && !isIframe(content)" class="thumbnail">
+		<!-- content.embed_thumbnail for non-Image/Video/Iframe-->
+		<!-- <div v-if="content.embed_thumbnail && !isImage(content) && !isVideo(content) && !isIframe(content)" class="thumbnail">
 			<img v-if="content.title == null" v-bind:src="content.embed_thumbnail"/>
 
 			<a v-else v-bind:href="content.url" target="_blank">
 				<img v-bind:src="content.embed_thumbnail"/>
 			</a>
-		</div>
+		</div> -->
 
-		<div class="title">
-			<a v-if="content.url != null" v-bind:href="content.url" target="_blank">{{ content.title | safe }}</a>
-			<span v-else>{{ content.title | safe }}</span>
-		</div>
+		<!-- url link -->
+		<a-entity class="title">
+			<!-- <a v-if="content.url != null" v-bind:href="content.url" target="_blank">{{ content.title | safe }}</a>
+			<span v-else>{{ content.title | safe }}</span> -->
+			<a-entity scale="2 2 1"
+                  :text="this.textString(content.title)"
+                   position="0 -.6 0"/>
+		</a-entity>
 
-		<div v-if="content.text != null" class="text">
-		<!--{% if text_truncated %}-->
-			<!--<a v-if="content.url && content.title == null" class="truncated" href="{{ url }}" target="_blank">{{ text_truncated | safe }}</a>-->
-		<!--{% endif %}-->
-		<a v-if="content.url && content.title == null" class="full" v-bind:href="content.url" target="_blank">{{ content.text | safe }}</a>
+		<!-- contet.text -->
+		<a-entity v-if="content.text != null" class="text">
+			<!--{% if text_truncated %}-->
+				<!--<a v-if="content.url && content.title == null" class="truncated" href="{{ url }}" target="_blank">{{ text_truncated | safe }}</a>-->
+			<!--{% endif %}-->
+			<!-- <a v-if="content.url && content.title == null" class="full" v-bind:href="content.url" target="_blank">{{ content.text | safe }}</a> -->
 
-		<!--{% if text_truncated %}-->
-			<!--<pre class="truncated">{{ text_truncated | safe }}</pre>-->
-		<!--{% endif %}-->
-		<pre v-else class="full">{{ content.text | safe }}</pre>
-			<!--<div class="expand">More</div>-->
-		</div>
+			<a-entity scale="2 2 1"
+                  :text="this.textString(content.text)"
+                   position="0 -.8 0"/>
+			<!--{% if text_truncated %}-->
+				<!--<pre class="truncated">{{ text_truncated | safe }}</pre>-->
+			<!--{% endif %}-->
+			<!-- <pre v-else class="full">{{ content.text | safe }}</pre> -->
+				<!--<div class="expand">More</div>-->
+		</a-entity>
 
-		<div class="tagging">
-			<div class="tags">
-				<span v-for="tag in content.tags">#{{ tag }}</span>
-			</div>
-		</div>
+		<a-entity class="tagging">
+			<a-entity class="tags">
+				<a-entity v-for="tag in content.tags"
+					:key="tag"
+					scale="2 2 1"
+					:text="tag">
+				</a-entity>
+			</a-entity>
+		</a-entity>
 	</a-entity>
 </template>
 
 <script>
 console.log("from carousel-item.vue <script>")
 export default {
-    props: ['obj'],
+	props: ['content', 'connection'],
+	
+	computed: {
+        imageMaterial: function () {
+            return 'src: #image-' + this.content.id + '; side: double'
+        }
+    },
 
     methods: {
         textString: function (value) {
@@ -89,7 +97,7 @@ export default {
     },
 
     mounted () {
-        //console.log(this.obj.id)
+        //console.log(this.content.id)
     }
   }
 </script>
--- components/xr/src/components/carousel/gallery-carousel.vue
+++ components/xr/src/components/carousel/gallery-carousel.vue
@@ -5,11 +5,17 @@
                         layout="type: line; margin: 4"
                         rotation="0 90 0"
                         position="-3.8 1 0">
-                <carouselItem v-for="wimage of $store.state.objects.content.slice(0, $store.state.objects.content.length/2)"
+                <carouselContent v-for="con of  $store.state.objects.content.slice(0, $store.state.objects.content.length/2)"
+                    :key="con.id"
+                    :content="con"
+                    :connection="con.connection"
+                    rotation="0 0 0">
+                </carouselContent>
+                <!-- <carouselItem v-for="wimage of $store.state.objects.content.slice(0, $store.state.objects.content.length/2)"
                             :key="wimage.id"
                             :obj='wimage'
                             rotation="0 0 0">
-                </carouselItem>
+                </carouselItem> -->
         </a-entity>
         <!-- Carousel right -->
         <a-entity class="gallery-carousel-right"
@@ -27,11 +33,12 @@
 
 <script>
 
-import carouselItem from "./components/carousel-item.vue"
+import carouselItem from "./components/carousel-item.vue";
+import carouselContent from "./components/carousel-content-item.vue";
 
-import Vue from 'vue'
+import Vue from 'vue';
 
-console.log("from carousel.vue <script>")
+console.log("from carousel.vue <script>");
 export default {
     // data () {
     //     return {
@@ -39,7 +46,8 @@ export default {
     //     }
     // },
     components: {
-        carouselItem
+        carouselItem,
+        carouselContent
     },
     mounted () {
         //debugger;
