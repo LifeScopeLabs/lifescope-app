8a7e0b61c98bb297bac6c02cf191422d4854d677
William Broza <lbroza@bitscoop.com>
2018-08-10 19:08:15 -0700

    Updated Login help.

--- components/modals/login-help.vue
+++ components/modals/login-help.vue
@@ -11,11 +11,11 @@
                 <p>Just click on any service, Connect to it, and authorize LifeScope to access your data.</p>
 
                 <h3>Do I change authorization permissions when logging in?</h3>
-                <p>Yes, your data authorizaiton selection for a service is updated every time you log in.</p>
+                <p>No, your data authorizaiton selection for a service is not updated if you have already created an account. You must change your scope authorizaition choices in Menu > Settings > Connections.</p>
 
                 <h3>Why don't all my Connections appear in the same account?</h3>
                 <p>In order to associate multiple Connections with the same LifeScope account, you must be logged in to LifeScope and then must Connect those service providers from the 'Add Connections' page in the App Menu.</p>
-                <p>If you sign up using one service, log out, then sign up using a different service, we will have ended up creating two separate LifeScope accounts for you.</p>
+                <p>If you sign up using one service, log out, then sign up using a different service, this will creating two separate LifeScope accounts.</p>
             </div>
         </div>
     </div>
