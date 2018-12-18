5d1bf041cc9602ad43a8f53562ef2c9e39725c63
Kyle Baran <kbaran@bitscoop.com>
2018-12-17 18:51:23 -0800

    Updated FA to 5.6.1.
    Fixed a bunch of broken icons and removed ones that don't seem to exist at all.
    
    Fixed improper prefix on saved icon text.
    
    Made icon box 200px high and scrollable on mobile.

--- nuxt.config.js
+++ nuxt.config.js
@@ -237,8 +237,8 @@ module.exports = {
 			},
 			{
 				rel: 'stylesheet',
-				href: 'https://pro.fontawesome.com/releases/v5.5.0/css/all.css',
-				integrity: "sha384-j8y0ITrvFafF4EkV1mPW0BKm6dp3c+J9Fky22Man50Ofxo2wNe5pT1oZejDH9/Dt",
+				href: 'https://pro.fontawesome.com/releases/v5.6.1/css/all.css',
+				integrity: "sha384-NJXGk7R+8gWGBdutmr+/d6XDokLwQhF1U3VA7FhvBDlOq7cNdI69z7NQdnXxcF7k",
 				crossorigin: "anonymous"
 			},
 			{
